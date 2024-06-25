84a3a08724adf90759dc09abaffb1888b8f7c5c5
Ulisses Junior <ulissesjunior2005@gmail.com>
2024-06-25 02:29:35 +0000

    feat: create cards page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
-// import CardsPage from './pages/CardsPage';
+import CardsPage from './pages/CardsPage';
 import RegisterPage from './pages/RegisterPage';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path='/cadastro' element={<RegisterPage />} />
-      {/* <Route path="/cards" element={<CardsPage />} /> */}
-    </Routes>
+    <>
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/cadastro" element={<RegisterPage />} />
+        <Route path="/cartas" element={<CardsPage />} />
+      </Routes>
+    </>
   );
 };
 
